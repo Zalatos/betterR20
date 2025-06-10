91f24761cbd413b31e4d9807aaf70baf1b6cb5e5
Ozzy <Mohamed.Osman@derivco.co.za>
2025-06-10 10:51:04 +0200

    feat: added support for cache lookup for separate tampermonkey script to push to

--- js/5etools-main.js
+++ js/5etools-main.js
@@ -313,7 +313,7 @@ const betteR205etoolsMain = function () {
 		{name: "bestiary index", url: `${MONSTER_DATA_DIR}index.json`},
 		{name: "bestiary fluff index", url: `${MONSTER_DATA_DIR}fluff-index.json`},
 		{name: "bestiary metadata", url: `${MONSTER_DATA_DIR}legendarygroups.json`},
-		// {name: "adventures index", url: `${DATA_URL}adventures.json`},  // error retrieving from 5e.tools
+		{name: "adventures index", url: `${DATA_URL}adventures.json`},  // error retrieving from 5e.tools
 		{name: "base items", url: `${DATA_URL}items-base.json`},
 		{name: "item modifiers", url: `${DATA_URL}roll20-items.json`},
 	];
